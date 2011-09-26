e3de495f15b677b8a0ee9d84bf1e1b2e8cd8748d
Миша Сидоров <sidorov@ek.apress.ru>
2011-09-26 18:00:44 +0600

    Fixed AJAX dataType for as_action links

--- lib/active_scaffold/helpers/view_helpers.rb
+++ lib/active_scaffold/helpers/view_helpers.rb
@@ -167,9 +167,12 @@ module ActiveScaffold
         html_options[:method] = link.method if link.method != :get
 
         html_options['data-confirm'] = link.confirm(record.try(:to_label)) if link.confirm?
-        html_options['data-position'] = link.position if link.position and link.inline?
-        html_options[:class] += ' as_action' if link.inline?
-        html_options['data-action'] = link.action if link.inline?
+        if link.inline?
+          html_options['data-position'] = link.position if link.position
+          html_options[:class] += ' as_action'
+          html_options['data-action'] = link.action
+          html_options['data-type'] = 'html xml'
+        end
         if link.popup?
           html_options['data-popup'] = true
           html_options[:target] = '_blank'
